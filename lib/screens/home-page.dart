4dd12f2a478a5334c58d2ed7b550cc91e936a6ac
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-03-17 09:39:51 -0300

    wireframe da primeira tela

---
+++ lib/screens/home-page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(99, 176, 205, 100),
+        title: Text("Clima Bike"),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.white,
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            Padding(padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  suffixIcon: const Icon(Icons.search),
+                  contentPadding: const EdgeInsets.all(15.0),
+                  hintText: Text("Cidade de Partida").data,
+                )
+              )
+            ),
+            Padding(padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      suffixIcon: const Icon(Icons.search),
+                      contentPadding: const EdgeInsets.all(15.0),
+                      hintText: Text("Cidade de Chegada").data,
+                    )
+                )
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 25, 10, 0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    padding: const EdgeInsets.all(13),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  child: const Text("Calcular", style: TextStyle(color: Colors.black),),
+                ),
+              ),
+            )
+          ],
+        )
+      )
+    );
+  }
+}
