2441c70f7484af1ff07313357743b59011ec2e7f
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-03-21 18:44:22 -0300

    correção no town

--- lib/model/city-counter.dart
+++ lib/model/city-counter.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 class CityCounter {
   static Future<List<String>> getCityCounter(List<List<double>> coordinaties) async {
     List<String> citiesFetch = [];
-    List<List<double>> waypoints = getCityNames(coordinaties, 90); // Agora retorna List<List<double>>
+    List<List<double>> waypoints = getCityNames(coordinaties, 90);
 
     for (var coord in waypoints) {
       double lat = coord[1];
@@ -20,10 +20,14 @@ class CityCounter {
       if (response.statusCode == 200) {
         final result = jsonDecode(response.body);
         print(result);
-        if (result["address"] != null && result["address"]["city"] != null) {
-          citiesFetch.add(result["address"]["city"]);
+        if (result["address"] != null) {
+          String? city = result["address"]["city"] ?? result["address"]["town"];
+          if (city != null) {
+            citiesFetch.add(city);
+          }
         }
       }
+
     }
 
     print(citiesFetch);
@@ -34,9 +38,13 @@ class CityCounter {
     List<List<double>> waypoints = [];
 
     for (int i = 0; i < coordinaties.length; i += ratingSearch) {
-      waypoints.add(coordinaties[i]); // Pega apenas um item e pula ratingSearch
+      waypoints.add(coordinaties[i]);
     }
-
+    
+    if  (waypoints.last != coordinaties.last) {
+      waypoints.add(coordinaties.last);
+    }
     return waypoints;
   }
+
 }
